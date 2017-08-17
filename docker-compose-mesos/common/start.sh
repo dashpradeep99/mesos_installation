3c629e96995a9856c843f227b7b6c1f3c920133d
mhfnoyon@gmail.com <mhfnoyon@gmail.com>
2017-08-17 17:42:46 +0600

    docker compose changed

--- docker-compose-mesos/common/start.sh
+++
@@ -1,7 +0,0 @@
-mkdir /root/.ssh -p
-cp /root/common/rsa/*.* /root/.ssh/ -f
-cat /root/common/rsa/id_rsa.pub >> /root/.ssh/authorized_keys
-chmod 600 /root/.ssh/authorized_keys
-#sed -i 's/prohibit/without/g' /etc/ssh/sshd_config
-/etc/init.d/ssh start
-sleep infinity
